61b5ed12d7062949b916e92f6e1e191776942118
Sahani <sahani070797@gmail.com>
2022-12-13 12:36:41 +0100

    Added Surface with new background colour

--- app/src/main/java/com/example/quizit/MainActivity.kt
+++ app/src/main/java/com/example/quizit/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.quizit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.Surface
 import com.example.quizit.presentation.navigation.Navigation
+import com.example.quizit.ui.theme.GreyBG
+import com.example.quizit.ui.theme.QuizITTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+            QuizITTheme{
+                Surface(color = GreyBG){
+                    Navigation()
+                }
+            }
         }
     }
 }
