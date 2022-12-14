e053f7734f17623e74eb26cf70a086026bfd0d9a
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2022-12-14 17:30:38 +0100

    Added Settings object

---
+++ app/src/main/java/com/example/quizit/domain/util/Settings.kt
@@ -0,0 +1,12 @@
+package com.example.quizit.domain.util
+
+/**
+ * Settings object to store API query parameters and players name
+ */
+object Settings {
+
+    lateinit var name: String
+    lateinit var category: String
+    lateinit var difficulty: String
+    lateinit var  limit: String
+}
