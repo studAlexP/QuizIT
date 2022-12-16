a44617f799c4ab3eb6d071020d6d039d4b20b744
Sahani <sahani070797@gmail.com>
2022-12-16 09:56:13 +0100

    import for CategorieScreen added

--- app/src/main/java/com/example/quizit/presentation/screens/categories/CategoriesScreen.kt
+++ app/src/main/java/com/example/quizit/presentation/screens/categories/CategoriesScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.quizit.presentation.widget.CategoryCard
 
 
 @Composable
