ca34761cd7cd90d00c2ea2579b276deb6ee93092
studAlexP <alexander.pruka@stud.fh-campuswien.ac.at>
2023-01-04 17:36:15 +0100

    Added viewmodel access

--- app/src/main/java/com/example/quizit/presentation/screens/quiz/QuizScreen.kt
+++ app/src/main/java/com/example/quizit/presentation/screens/quiz/QuizScreen.kt
@@ -1,31 +1,45 @@
 package com.example.quizit.presentation.screens.quiz
 
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.runtime.Composable
+import android.app.Activity
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.quizit.di.QuizViewModelFactoryProvider
+import com.example.quizit.domain.util.Settings
 import com.example.quizit.presentation.navigation.Screen
+import com.example.quizit.presentation.viewmodel.QuizViewModel
+import com.example.quizit.presentation.widget.DisplayQuestion
+import com.example.quizit.presentation.widget.LoadingAnimation
+import dagger.hilt.android.EntryPointAccessors
 
 @Composable
 fun QuizScreen(
     navController: NavController
 ) {
-    TopAppBar(
-        title = { Text(text = "Linux") },
-        actions = {
-            IconButton(onClick = { /* do something */ }) {
-                Icon(
-                    imageVector = Icons.Default.Close,
-                    contentDescription = "close"
-                )
-            }
-        }
+    val factory = EntryPointAccessors.fromActivity(
+        LocalContext.current as Activity,
+        QuizViewModelFactoryProvider::class.java
+    ).quizViewModelFactory()
+
+    val viewModel: QuizViewModel = viewModel(
+        factory = QuizViewModel.provideQuizViewModelFactory(
+            factory = factory,
+            category = "",
+            difficulty = Settings.difficulty,
+            limit = "10"
+        )
     )
+
+    val questions = viewModel.questions.collectAsState()
+
+    if (questions.value == null) {
+        LoadingAnimation()
+    } else {
+        DisplayQuestion(quiz = questions.value!!) {
+            navController.navigate(Screen.StartScreen.route)
+        }
+    }
 }
 
 
